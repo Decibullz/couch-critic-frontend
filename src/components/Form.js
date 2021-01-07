211b106fc0633b5e767413228e378346eef6e41f
Cody Snell <codyjsnell@yahoo.com>
2021-01-06 23:04:09 -0600

    finalizes functionality of app

---
+++ src/components/Form.js
@@ -0,0 +1,85 @@
+import { useState, useEffect } from 'react';
+import Input from './Input.js';
+
+function Form(props) {
+    const [formState, setFormState] = useState({
+        title:'',
+        image:'',
+        rating:'',
+        review:'',
+        watch:'',
+    });
+
+    useEffect(()=>{
+        if(props.review){
+            setFormState({
+                title: props.review.title,
+                image: props.review.image,
+                rating: props.review.rating,
+                review: props.review.review,
+                watch: props.review.watch,
+                id: props.review.id,
+            })
+        }
+    },[props.review])
+
+    function handleChange(event) {
+        setFormState(prevState => ({
+        ...prevState,
+        [event.target.id] : event.target.value
+        }));
+    }
+
+    function handleSubmit(event){
+        event.preventDefault();
+        if(props.review) formState.id=props.review.id
+        props.handleSubmit(event, formState);
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <Input
+                handleChange={handleChange}
+                name="title"
+                placeholder="Title"
+                type="text"
+                value={formState.title}
+                id="title"
+            />
+            <Input
+                handleChange={handleChange}
+                name="image"
+                placeholder="Image Link"
+                type="text"
+                value={formState.image}
+                id="image"
+            />
+            <Input
+                handleChange={handleChange}
+                name="rating"
+                placeholder="Rating"
+                type="select"
+                value={formState.rating}
+                id="rating"
+            />
+            <Input
+                handleChange={handleChange}
+                name="review"
+                placeholder="Review"
+                type="text-area"
+                value={formState.review}
+                id="review"
+            />
+            <Input
+                handleChange={handleChange}
+                name="watch"
+                placeholder="Where to watch"
+                type="text"
+                value={formState.watch}
+                id="watch"
+            />
+            <input type="submit" value={props.review ? 'Edit review' : 'Add review'}/>
+        </form>
+    );
+}
+
+export default Form;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header(props) {
+    return (
+        <header>
+            <h1>Couch Critic</h1>
+        </header>
+    );
+}
+
+export default Header;
